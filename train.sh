d4c77f218e3125f5faa84578cfbb40f476b2cc9d
Lim Swee Kiat <limsweekiat@gmail.com>
2020-09-12 17:25:20 +0800

    Fix sed command

--- train.sh
+++ train.sh
@@ -6,15 +6,15 @@ git clone https://github.com/AlexeyAB/darknet.git
 sed -i '1s/.*/GPU=1/' darknet/Makefile
 sed -i '2s/.*/CUDNN=1/' darknet/Makefile
 sed -i '7s/.*/LIBSO=1/' darknet/Makefile
-# sed -i '35s/.*/ARCH= -gencode arch=compute_75,code=[sm_75,compute_75]/' darknet/Makefile
+sed -i '35s/.*/ARCH= -gencode arch=compute_75,code=[sm_75,compute_75]/' darknet/Makefile
 cd darknet && make
 
-sed -i '20s/.*/max_batches=10' cfg/yolov4-tiny-custom.cfg
-sed -i '22s/.*/steps=8,9' cfg/yolov4-tiny-custom.cfg
-sed -i '212s/.*/filters=27' cfg/yolov4-tiny-custom.cfg
-sed -i '220s/.*/classes=4' cfg/yolov4-tiny-custom.cfg
-sed -i '263s/.*/filters=27' cfg/yolov4-tiny-custom.cfg
-sed -i '269s/.*/classes=4' cfg/yolov4-tiny-custom.cfg
+sed -i '20s/.*/max_batches=10/' cfg/yolov4-tiny-custom.cfg
+sed -i '22s/.*/steps=8,9/' cfg/yolov4-tiny-custom.cfg
+sed -i '212s/.*/filters=27/' cfg/yolov4-tiny-custom.cfg
+sed -i '220s/.*/classes=4/' cfg/yolov4-tiny-custom.cfg
+sed -i '263s/.*/filters=27/' cfg/yolov4-tiny-custom.cfg
+sed -i '269s/.*/classes=4/' cfg/yolov4-tiny-custom.cfg
 wget https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v4_pre/yolov4-tiny.conv.29
 
 #wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1dfD1k77WL_xFmYXUOZOAqTnRUrtF2FaR' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1dfD1k77WL_xFmYXUOZOAqTnRUrtF2FaR" -O yolo_data.zip && rm -rf /tmp/cookies.txt
