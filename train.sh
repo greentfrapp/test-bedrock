27008ddefb15cb837b3f4bae113120747dada9eb
Lim Swee Kiat <limsweekiat@gmail.com>
2020-09-12 23:33:35 +0800

    Fix params in bash script

--- train.sh
+++ train.sh
@@ -9,18 +9,18 @@ sed -i '7s/.*/LIBSO=1/' darknet/Makefile
 sed -i '35s/.*/ARCH= -gencode arch=compute_75,code=[sm_75,compute_75]/' darknet/Makefile
 cd darknet && make
 
-sed -i '20s/.*/max_batches=$NUM_STEPS/' cfg/yolov4-tiny-custom.cfg
-sed -i '22s/.*/steps=$(($NUM_STEPS / 10 * 8)),$(($NUM_STEPS / 10 * 9))/' cfg/yolov4-tiny-custom.cfg
-sed -i '212s/.*/filters=$((($NUM_CLASSES + 5) * 3))/' cfg/yolov4-tiny-custom.cfg
-sed -i '220s/.*/classes=$NUM_CLASSES/' cfg/yolov4-tiny-custom.cfg
-sed -i '263s/.*/filters=$((($NUM_CLASSES + 5) * 3))/' cfg/yolov4-tiny-custom.cfg
-sed -i '269s/.*/classes=$NUM_CLASSES/' cfg/yolov4-tiny-custom.cfg
+sed -i "20s/.*/max_batches=$NUM_STEPS/" cfg/yolov4-tiny-custom.cfg
+sed -i "22s/.*/steps=$(($NUM_STEPS / 10 * 8)),$(($NUM_STEPS / 10 * 9))/" cfg/yolov4-tiny-custom.cfg
+sed -i "212s/.*/filters=$((($NUM_CLASSES + 5) * 3))/" cfg/yolov4-tiny-custom.cfg
+sed -i "220s/.*/classes=$NUM_CLASSES/" cfg/yolov4-tiny-custom.cfg
+sed -i "263s/.*/filters=$((($NUM_CLASSES + 5) * 3))/" cfg/yolov4-tiny-custom.cfg
+sed -i "269s/.*/classes=$NUM_CLASSES/" cfg/yolov4-tiny-custom.cfg
 
 apt-get -y install wget
 wget https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v4_pre/yolov4-tiny.conv.29
 
 # Disable the following if using aws-production environment
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=$GOOGLE_FILE_ID' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$GOOGLE_FILE_ID" -O yolo_data.zip && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=$GOOGLE_FILE_ID" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$GOOGLE_FILE_ID" -O yolo_data.zip && rm -rf /tmp/cookies.txt
 apt-get -y install unzip
 unzip yolo_data.zip
 
